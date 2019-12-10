4149256e00bd0b9b215e0166a87eef40305de021
jeromed <jeromed@askia.com>
2019-12-10 12:12:41 +0100

    v3.0.6

--- resources/dynamic/Initializer.js
+++ resources/dynamic/Initializer.js
@@ -35,6 +35,7 @@ Next i1
     useSlider: {%= CurrentADC.PropValue("useSlider") %},
     headerFixed: {%= On(CurrentADC.PropValue("headerFixed") = "1", true, false)%},
     stepByStep: {%= On(CurrentADC.PropValue("stepByStep") = "yes", true, false)%},
+    useStepByStep: '{%= CurrentADC.PropValue("useStepByStep") %}',
     questions: [{% For i5 = 1 To columnNumber
     	column = CurrentADC.PropQuestion("questionCol" + i5)
             If ((column.Id = DK) and (i5 = 1)) Then
