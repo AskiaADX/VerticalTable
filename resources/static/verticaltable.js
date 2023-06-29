6763bb13ee363ea7c73dc529832c9f675747e4c5
prab.g <prabath@askia.com>
2023-06-29 19:11:05 +0530

    v3.1.3

--- resources/static/verticaltable.js
+++ resources/static/verticaltable.js
@@ -7,12 +7,13 @@
       var el = this;
       if (!document.documentElement.contains(el)) return null;
       do {
-        if (el.matches(s)) return el;
+        if (el.matches && el.matches(s)) return el;
+        if (el.msMatchesSelector && el.msMatchesSelector(s)) return el;
         el = el.parentElement || el.parentNode;
       } while (el !== null && el.nodeType === 1);
       return null;
     };
-  }
+  }    
 })();
 (function () {
   // Create a safe reference to the Underscore object for use below.
